b10a22e4648efd2034eb2783b6f467319a7ecf47
Melville <melville.leesy@gmail.com>
2025-08-20 17:22:43 +0800

    Echo

--- src/main/java/IDKName.java
+++ src/main/java/IDKName.java
@@ -1,22 +1,45 @@
+import java.util.Scanner;
+
 public class IDKName {
     private String name;
+    private Scanner scanner;
+    private final String line = "_".repeat(60);
 
     public IDKName() {
         this.name = "IDKName";
+        this.scanner = new Scanner(System.in);
     }
 
     private String greetings() {
-        String line = "_".repeat(60);
         return String.format("%s" +
                 "%nHello! I'm %s" +
                 "%nWhat can I do for you?" +
-                "%n%s" +
-                "%nBye. Hope to see you soon!" +
-                "%n%s", line, this.name, line, line);
+                "%n%s", this.line, this.name, this.line);
+    }
+
+    private String goodbye() {
+        return String.format("Bye. Hope to see you again!" +
+                "%n%s", this.line);
+    }
+
+    private void echo() {
+        String userInput;
+        while (true) {
+            System.out.print("Message Prompt (type bye to exit): ");
+            userInput = scanner.nextLine();
+            System.out.println(this.line);
+            if (userInput.toLowerCase().equals("bye")) {
+                break;
+            }
+            String message = String.format("%s%n%s", userInput, this.line);
+            System.out.println(message);
+        }
     }
 
     public static void main(String[] args) {
         IDKName chatbot = new IDKName();
         System.out.println(chatbot.greetings());
+        chatbot.echo();
+        System.out.println(chatbot.goodbye());
     }
 }
