f0c0906aadf1ed4ee2f90910be55166db5221770
Melville <melville.leesy@gmail.com>
2025-08-20 17:51:26 +0800

    Level 2: Add, List

--- src/main/java/IDKName.java
+++ src/main/java/IDKName.java
@@ -4,10 +4,14 @@ public class IDKName {
     private String name;
     private Scanner scanner;
     private final String line = "_".repeat(60);
+    private String[] list;
+    private int currentListPos;
 
     public IDKName() {
         this.name = "IDKName";
         this.scanner = new Scanner(System.in);
+        this.list = new String[100];
+        this.currentListPos = 0;
     }
 
     private String greetings() {
@@ -25,15 +29,27 @@ public class IDKName {
     private void echo() {
         String userInput;
         while (true) {
-            System.out.print("Message Prompt (type bye to exit): ");
+            System.out.print("Message Prompt ('bye': exit, 'list': list ");
             userInput = scanner.nextLine();
             System.out.println(this.line);
             if (userInput.toLowerCase().equals("bye")) {
                 break;
+            } else if (userInput.toLowerCase().equals("list")) {
+                for (int i = 0; i < this.currentListPos; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, this.list[i]));
+                }
+                System.out.println(this.line);
+            } else {
+                String message = String.format("added: %s%n%s", userInput, this.line);
+                this.addList(userInput);
+                System.out.println(message);
             }
-            String message = String.format("%s%n%s", userInput, this.line);
-            System.out.println(message);
         }
+    }
+
+    private void addList(String item) {
+        this.list[this.currentListPos] = item;
+        this.currentListPos++;
     }
 
     public static void main(String[] args) {
