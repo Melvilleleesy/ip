b898492fa9ceb7b6e260f0d2fbba1ef545f281bf
Melville <melville.leesy@gmail.com>
2025-08-24 01:33:19 +0800

    Add Level 7: Load and Save methods

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -8,4 +8,19 @@ public class Todo extends Task{
         String super_s = "[T]" + super.toString();
         return super_s;
     }
+
+    @Override
+    public String getTaskType() {
+        return "T";
+    }
+
+    @Override
+    public String getDueDate() {
+        return "";
+    }
+
+    @Override
+    public String[] getTimePeriod() {
+        return null;
+    }
 }
